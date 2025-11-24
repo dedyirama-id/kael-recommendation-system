7926531320d5dbcfca821c24e0c4154715bc5e13
dedyirama-id <dedyirama.ars@gmail.com>
2025-11-24 19:38:00 +0700

    chore: add docs for some method

--- src/main/java/org/kael/algorithms/Search.java
+++ src/main/java/org/kael/algorithms/Search.java
@@ -1,6 +1,12 @@
 package org.kael.algorithms;
 
 public class Search {
+  /**
+   *
+   * @param data Array of data to search
+   * @return Return found data or null if not found
+   * @throws Exception Throw any exception if process failed
+   */
   public static <T> T linearSearch(T[] data) throws Exception {
     throw new Exception("Method not implemented yet!");
   }
--- src/main/java/org/kael/algorithms/Sort.java
+++ src/main/java/org/kael/algorithms/Sort.java
@@ -1,7 +1,13 @@
 package org.kael.algorithms;
 
-public class Sort<T> {
-  public static <T> T[] selectionSort(T data) throws Exception {
+public class Sort {
+  /**
+   * Sort array of data using selection sort algorithm.
+   *
+   * @param data Random array of data to be sorted
+   * @throws Exception Throw any exception if process failed
+   */
+  public static <T> void selectionSort(T[] data) throws Exception {
     throw new Exception("Method not implemented yet!");
   }
 }
