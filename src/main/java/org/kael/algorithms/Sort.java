221c11212175a5aea22f4e04097d7d7a4035ef68
Christabel Arrowina <christabelarrowina4@gmailm.com>
2025-11-27 00:43:33 +0700

    Selection Sort

--- src/main/java/org/kael/algorithms/Sort.java
+++ src/main/java/org/kael/algorithms/Sort.java
@@ -1,13 +1,36 @@
 package org.kael.algorithms;
 
 public class Sort {
-  /**
-   * Sort array of data using selection sort algorithm.
-   *
-   * @param data Random array of data to be sorted
-   * @throws Exception Throw any exception if process failed
-   */
-  public static <T> void selectionSort(T[] data) throws Exception {
-    throw new Exception("Method not implemented yet!");
-  }
+
+    /**
+     * Sort array of data using selection sort algorithm.
+     *
+     * @param data Random array of data to be sorted. Element should implement Comparable interface.
+     * @throws Exception Throw any exception if process failed
+     */
+    public static <T extends Comparable<T>> void selectionSort(T[] data) throws Exception {
+        if (data == null) {
+            throw new Exception("Data cannot be null");
+        }
+
+        int n = data.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Cari elemen terkecil mulai dari i+1
+            for (int j = i + 1; j < n; j++) {
+                if (data[j].compareTo(data[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap hanya jika index berubah
+            if (minIndex != i) {
+                T temp = data[i];
+                data[i] = data[minIndex];
+                data[minIndex] = temp;
+            }
+        }
+    }
 }
